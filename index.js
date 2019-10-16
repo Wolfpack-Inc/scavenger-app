fcc19378c051cafdfbcc31f97c3025ebff57a436
Jules Huisman <huisman.jules@gmail.com>
2019-10-16 17:48:35 +0200

    Added google maps

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+/** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/components/App/App';
+import { name as appName } from './app.json';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Remote debugger']);
 
 AppRegistry.registerComponent(appName, () => App);

6e491f63b048fc111a6485dab1a159041de573a4
Jules Huisman <huisman.jules@gmail.com>
2019-11-04 14:09:38 +0100

    Added the street of the image when searching

--- src/components/CurrentImage/CurrentImage.js
+++ src/components/CurrentImage/CurrentImage.js
@@ -23,46 +23,63 @@ export class CurrentImage extends Component {
 
         Animated.timing(this.open, {
             toValue: this.state.open ? 1 : 0,
-            duration: 240,
-            // useNativeDriver: true
+            duration: 240
         }).start();
     }
 
     render() {
         const { image } = this.props;
         const { open } = this.state;
-        const { url } = image;
+        const { url, street } = image;
 
         return (
-            <Animated.View 
-                styleName='touch-wrapper'
-                style={{
-                    width: this.open.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [140, width]
-                    }),
-                    borderRadius: this.open.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [10, 0]
-                    }),
-                    height: this.open.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [200, height]
-                    }),
-                    margin: this.open.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [15, 0]
-                    }),
-                }}>
-                    
-                <TouchableNativeFeedback 
-                    styleName='touchable' 
-                    onPress={this.handleClick.bind(this)}>
-                    <Image
-                        styleName='image'
-                        source={{ uri: url }}/>
-                </TouchableNativeFeedback>
-            </Animated.View>
+            <Fragment>
+                <Location street={street} />
+                <Animated.View 
+                    styleName='touch-wrapper'
+                    style={{
+                        width: this.open.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [140, width]
+                        }),
+                        borderRadius: this.open.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [10, 0]
+                        }),
+                        height: this.open.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [200, height]
+                        }),
+                        margin: this.open.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [15, 0]
+                        }),
+                    }}>
+                        
+                    <TouchableNativeFeedback 
+                        styleName='touchable' 
+                        onPress={this.handleClick.bind(this)}>
+                        <Image
+                            styleName='image'
+                            source={{ uri: url }}/>
+                    </TouchableNativeFeedback>
+                </Animated.View>
+            </Fragment>
+        )
+    }
+}
+
+class Location extends Component {
+    render() {
+        const { street } = this.props;
+
+        return (
+            <View
+                styleName='location'>
+                <Text
+                    styleName='location-label'
+                    numberOfLines={1}>{street}</Text>
+            </View>
         )
     }
 }
